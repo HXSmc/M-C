7063c39f089a7d4ab7f1820e5b6f345c8e2b3fdf
HXSmc <74503116+HXSmc@users.noreply.github.com>
2021-12-29 00:21:55 +0300

    trying to implement the NLP training

---
+++ skills/collection/activation.py
@@ -0,0 +1,18 @@
+class ActivationSkills():
+
+    @classmethod
+    def enable_assistant(cls, **kwargs):
+        """
+        Plays activation sound and creates the assistant response according to the day hour.
+        """
+
+        
+
+    @classmethod
+    def disable_assistant(cls, **kwargs):
+        """
+        - Clear console
+        - Shutdown the assistant service
+        """
+
+        
