89603587c8678194bdf244de39f977d9075eff4c
HXSmc <74503116+HXSmc@users.noreply.github.com>
2021-12-29 07:59:01 +0300

    update training.py

--- skills/training.py
+++ skills/training.py
@@ -1,12 +1,3 @@
-import json
-import string
-import random 
-import nltk
-import numpy as np
-from nltk.stem import WordNetLemmatizer 
-import tensorflow as tf 
-from tensorflow.keras import Sequential 
-from tensorflow.keras.layers import Dense, Dropout
-
-nltk.download("punkt")
-nltk.download("wordnet")
+from neuralintents import GenericAssistant
+import sys
+from MC import wakecommand
